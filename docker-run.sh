ad6cffe97477f67dffe6db8f88f16c98bc97945a
Vinod Kumar Kayartaya <vinod@vinod.co>
2025-05-13 16:01:29 +0530

    removed unwanted files

--- docker-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build and run the Docker container
-echo "Building and starting the contact service..."
-docker-compose up --build -d
-
-echo "Service is starting. You can access it at http://localhost:8080/api/contacts"
-echo "To view logs: docker-compose logs -f"
-echo "To stop the service: docker-compose down" 
